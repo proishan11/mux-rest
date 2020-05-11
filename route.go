599888801a2923951951852ef5bd84cac4ce26e8
Ishan Singh <ankit.singh040@gmail.com>
2020-05-11 17:20:23 +0530

    add POST method to add a single post

--- route.go
+++ route.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -32,10 +33,35 @@ func getPosts(response http.ResponseWriter, req *http.Request) {
 	response.Header().Set("Content-type", "application/json")
 	result, err := json.Marshal(posts)
 	if err != nil {
+		log.Println("Error occured while marshalling posts")
 		// take an integer parameter
 		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(`{"error": "Error occurred while marshaling posts"}`))
+		response.Write([]byte(`{"error": "Error occurred while marshalling posts"}`))
 		return
+	}
+
+	response.WriteHeader(http.StatusOK)
+	response.Write(result)
+}
+
+func addPost(response http.ResponseWriter, req *http.Request) {
+	response.Header().Set("Content-type", "application/json")
+	var post Post
+	err := json.NewDecoder(req.Body).Decode(&post)
+	if err != nil {
+		log.Println("Error occurred while unmarshalling the request")
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"error": "Error occurred while unmarshalling the request"}`))
+		return
+	}
+
+	post.ID = len(posts) + 1
+	posts = append(posts, post)
+	result, err := json.Marshal(post)
+	if err != nil {
+		log.Println("Error occurred while marshalling the response")
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{"error": "Error occurred while marshalling the request"}`))
 	}
 
 	response.WriteHeader(http.StatusOK)
--- server.go
+++ server.go
@@ -16,6 +16,7 @@ func main() {
 		fmt.Fprintf(response, "Server up and running")
 	})
 	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", addPost).Methods("POST")
 
 	log.Println("Server listening on port ", port)
 	log.Fatalln(http.ListenAndServe(port, router))
