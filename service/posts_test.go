0f5cd6d7fbd91b8c7d415febadb38b2dbf12f28a
Ishan Singh <ankit.singh040@gmail.com>
2020-05-22 13:23:18 +0530

    Add tests for post service

--- service/posts_test.go
+++ service/posts_test.go
@@ -6,7 +6,56 @@ import (
 	"github.com/proishan11/mux-rest/models"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (mock MockRepository) Save(post *models.Post) (*models.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.(*models.Post), args.Error(1)
+}
+
+func (mock MockRepository) FindAll() ([]models.Post, error) {
+	args := mock.Called()
+	result := args.Get(0)
+	return result.([]models.Post), args.Error(1)
+}
+
+func TestFindAll(t *testing.T) {
+	mockRepo := new(MockRepository)
+	post := models.Post{ID: 1, Title: "A", Text: "B"}
+	// Add expectated results when the given function get called
+	mockRepo.On("FindAll").Return([]models.Post{post}, nil)
+
+	testService := NewPostService(mockRepo)
+	result, err := testService.FindAll()
+
+	var ID int64 = 1
+	mockRepo.AssertExpectations(t)
+	assert.Equal(t, ID, result[0].ID)
+	assert.Equal(t, "A", result[0].Title)
+	assert.Equal(t, "B", result[0].Text)
+	assert.Nil(t, err)
+}
+
+func TestSave(t *testing.T) {
+	mockRepo := new(MockRepository)
+	post := models.Post{Title: "A", Text: "B"}
+	mockRepo.On("Save").Return(&post, nil)
+
+	testService := NewPostService(mockRepo)
+	result, err := testService.Create(&post)
+
+	mockRepo.AssertExpectations(t)
+	assert.NotNil(t, result.ID)
+	assert.Equal(t, "A", result.Title)
+	assert.Equal(t, "B", result.Text)
+	assert.Nil(t, err)
+}
 
 func TestValidateEmptyPost(t *testing.T) {
 	testService := NewPostService(nil)
